d2284912538dc19ef3778e920d8a175cbf03c583
suneetha <suneethakodali84@gmail.com>
2021-04-01 13:52:55 +0100

    changed files from developer branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from './components/Button'
 import './css/style.css'
-// function App() {
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +22,6 @@ class App extends Component {
     if (["/", "-", "+", "*"].indexOf(symbol) > -1) {
       let { previous } = this.state;
       previous.push(this.state.current + symbol);
-      // this.setState({ previous: previous })
       this.setState({ previous, nextIsReset: true });
     } else {
       if ((this.state.current === "0" && symbol !== ".") || this.state.nextIsReset) {
@@ -66,7 +64,6 @@ class App extends Component {
       <div className="App">
         {this.state.previous.length > 0 ? <div className="floaty-last">{this.state.previous[this.state.previous.length - 1]}</div> : null}
         <input className="result" type="text" value={this.state.current} />
-        {/* {buttons.map(function(){})} */}
         {buttons.map((btn, i) => {
           return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
         })}
